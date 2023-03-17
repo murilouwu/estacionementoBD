212c224f5debedc6a43731e5900427b260c33e5b
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2023-03-17 11:35:38 -0300

    version 2

--- java.js
+++ java.js
@@ -14,3 +14,20 @@ function iconMod(btn, id, fun){
 function subir(){
 	document.querySelector('#sistem').scrollTo(0, 0);
 }
+
+function Modal(modal, form, fun){
+	let divs = [
+		document.querySelector(modal),
+		document.querySelector(form)
+	];
+	if(fun==1){
+		let frml = form=='#frm1'? '#frm2':'#frm1';
+		let div document.querySelector(frm);
+		div.style.display = 'none';
+		divs[0].style.display = 'none';
+		divs[1].style.display = 'none';
+	}else{
+		divs[0].style.display = 'flex';
+		divs[1].style.display = 'flex';
+	}
+}
